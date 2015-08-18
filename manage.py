4388a804f82962659fdb85c7f41dba6f08d6832c
John Krauss <irving.krauss@gmail.com>
2015-08-18 17:49:20 -0400

    in-progress csv adding & pinging users to create password

--- manage.py
+++ manage.py
@@ -7,14 +7,22 @@ Scripts to run the server and perform maintenance operations
 from app import mail, models
 from app.factory import create_app
 from app.models import db, User, UserSkill
+from app.utils import csv_reader
 
 from flask_alchemydumps import AlchemyDumps, AlchemyDumpsCommand
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_security.utils import encrypt_password
+from flask_security.recoverable import send_reset_password_instructions
+
+from random import choice
+from sqlalchemy.exc import IntegrityError
 
 import json
+import os
+import string
 import subprocess
+
 
 app = create_app() #pylint: disable=invalid-name
 migrate = Migrate(app, db) #pylint: disable=invalid-name
@@ -41,8 +49,9 @@ def translate():
     """
     Extract translation
     """
-    subprocess.check_call('pybabel extract -F app/babel.cfg -k lazy_gettext -o app/messages.pot app/',
-                          shell=True)
+    subprocess.check_call(
+        'pybabel extract -F app/babel.cfg -k lazy_gettext -o app/messages.pot app/',
+        shell=True)
     return 0
 
 
@@ -64,6 +73,46 @@ def drop_db(verbose=False):
     #db.engine.execute("create schema public;")
     #db.create_all()
     return 0
+
+
+@manager.command
+def add_fake_users(users_csv):
+    """
+    Add a bunch of fake users from a CSV.  Will set bogus passwords for them.
+    """
+    # TODO make sure each fake user is pre- confirmed, otherwise the password
+    # reset link we send them will require an email confirmation after the fact,
+    # confusing
+    # TODO add an auto-added flag (source)
+    for row in csv_reader(os.path.join('/noi', users_csv)):
+        row['password'] = ''.join(choice(string.letters + string.digits) for _ in range(20))
+        row['active'] = False  # TODO is this OK?
+        user = User(**row)  # pylint: disable=star-args
+        db.session.add(user)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            app.logger.warn('Could not add user with email "%s", as they are '
+                            'already in the database.', row['email'])
+
+
+@manager.command
+def send_bulk_password_reset_links(users_csv):
+    """
+    Send bulk password reset links for the included emails.  Argument is a CSV,
+    which will be read for an email column.
+    """
+    for row in csv_reader(os.path.join('/noi', users_csv)):
+        # TODO filter users we don't have in the DB
+        # TODO Make sure we only send bulk password resets to inactive users
+        # who have never confirmed their email (even better would be to add an
+        # auto-added flag and only send to those, then mark that we've sent
+        # them a reset link)
+        # TODO specialize the email sent so it's not "reset your password" but
+        # "Claim your NoI account" or somesuch, upon which password is set.
+        user = User.query.filter_by(email=row['email']).one()
+        send_reset_password_instructions(user)
 
 
 @manager.command
