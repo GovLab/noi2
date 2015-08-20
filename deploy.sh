b0ce495e374a54f0b20ebfeacc1313747b8aa3b9
John Krauss <irving.krauss@gmail.com>
2015-08-20 11:28:16 -0400

    include db upgrade in the generic run script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-./manage.sh db upgrade
 NOI_ENVIRONMENT=production docker-compose up > out.log 2> err.log &
--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -e
 
 if [ "$NOI_ENVIRONMENT" == production ]; then
-    cd /noi && gunicorn wsgi:application -b 0.0.0.0:5000
+    cd /noi
+    python manage.py db upgrade
+    gunicorn wsgi:application -b 0.0.0.0:5000
 elif [ "$NOI_ENVIRONMENT" == celery ]; then
     #cd /noi && celery worker -Q celery -A celery_core.celery --loglevel=debug
     #cd /noi && celery -A celery_core.celery beat --loglevel=debug
     cd /noi && celery -A celery_core.celery worker -B --loglevel=debug
 else
+    python /noi/manage.py db upgrade
     NOI_ENVIRONMENT=development python /noi/manage.py runserver --host 0.0.0.0
 fi
