839acf002a060cf44a93068f1ce49fc2cddd4a2a
Atul Varma <varmaa@gmail.com>
2015-09-17 18:28:36 -0400

    add trivial test suite to use test db.

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -1,6 +1,33 @@
+from flask import Flask
+from flask.ext.testing import TestCase
 from nose.tools import eq_
 
 from .. import models
+
+db = models.db
+
+class DbTest(TestCase):
+    def create_app(self):
+        app = Flask('test')
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        app.config['NOI_DEPLOY'] = '_default'
+        db.init_app(app)
+        return app
+
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+class UserDbTests(DbTest):
+    def test_ensure_deployment_has_a_default_setting(self):
+        u = models.User(email=u'a@example.org', password='a', active=True)
+        db.session.add(u)
+        db.session.commit()
+        eq_(u.deployment, '_default')
 
 def test_users_only_display_in_search_if_they_have_first_and_last_name():
     user = models.User()
