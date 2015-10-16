5744c4029ef0f39d778a05f5b6d9136e3029019d
Atul Varma <varmaa@gmail.com>
2015-10-16 13:07:52 -0400

    make l10n tests run a bit faster.

--- app/tests/test_l10n.py
+++ app/tests/test_l10n.py
@@ -11,13 +11,13 @@ def test_flask_security_strings_do_not_interpolate():
     eq_(unicode(app.config['SECURITY_MSG_EMAIL_ALREADY_ASSOCIATED'][0]),
         '%(email)s is already associated with an account.')
 
-class LocalizationTests(ViewTestCase):
-    def test_all_translations_exist(self):
-        dirs = os.listdir('/noi/app/translations')
-        for locale in l10n.TRANSLATIONS:
-            if locale not in dirs:
-                self.fail('Locale %s does not exist' % locale)
+def test_all_translations_exist():
+    dirs = os.listdir('/noi/app/translations')
+    for locale in l10n.TRANSLATIONS:
+        if locale not in dirs:
+            raise Exception('Locale %s does not exist' % locale)
 
+class LocalizationTests(ViewTestCase):
     def test_locale_is_en_by_default(self):
         res = self.client.get('/')
         assert '<html lang="en"' in res.data
