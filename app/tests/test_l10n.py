bafb594c75299c352580bdfeb6db4e5a65ef8b08
Atul Varma <varmaa@gmail.com>
2015-11-23 13:17:21 -0500

    Add regression test for #211. Fixes #212.

--- app/tests/test_l10n.py
+++ app/tests/test_l10n.py
@@ -1,4 +1,7 @@
 import os
+from babel.messages.pofile import read_po
+from babel.messages.catalog import TranslationError, Message
+from babel.messages import checkers
 from flask import Flask
 from nose.tools import eq_
 
@@ -35,3 +38,33 @@ class LocalizationTests(ViewTestCase):
             'Accept-Language': 'es'
         })
         assert '<html lang="es"' in res.data
+
+def check_sources_without_named_placeholders_work(catalog, msg):
+    if '%(' in msg.string and '%(' not in msg.id:
+        raise TranslationError("Translation contains named placeholder "
+                               "but source doesn't: %s" % msg.string)
+
+checkers.checkers.append(check_sources_without_named_placeholders_work)
+
+def test_checker_works():
+    m = Message(id=u'hello', string=u'hello %(num)d')
+    errors = [err for err in m.check()]
+    eq_(len(errors), 1)
+    eq_(str(errors[0]), 'Translation contains named placeholder but '
+                        'source doesn\'t: hello %(num)d')
+
+def _make_pofile_test(locale):
+    def test_pofile():
+        pofile = open(
+            '/noi/app/translations/%s/LC_MESSAGES/messages.po' % locale,
+            'r'
+        )
+        catalog = read_po(pofile)
+        errors = catalog.check()
+        eq_([error for error in errors], [])
+
+    test_pofile.__name__ = 'test_pofile_%s' % locale
+    globals()[test_pofile.__name__] = test_pofile
+
+for locale in l10n.TRANSLATIONS:
+    _make_pofile_test(locale)
