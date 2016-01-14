a25c07450b0dbf6289f77b85946b7834e7e29fb7
Atul Varma <varmaa@gmail.com>
2016-01-14 09:22:00 -0500

    Show timestamps in activity feed.

--- app/static/js/pages/activity.js
+++ app/static/js/pages/activity.js
@@ -4,6 +4,45 @@ $(function() {
     loadingHtml: '<div class="e-load-more">' +
                  pageConfig.LOADING_TEXT +
                  '</div>',
-    autoTrigger: false
+    autoTrigger: false,
+    callback: function() {
+      $('time', this).timeago();
+    }
   });
+
+  var lang = $('html').attr('lang');
+
+  // https://github.com/rmm5t/jquery-timeago/tree/master/locales
+  if (lang == 'es') {
+    // Spanish
+    jQuery.timeago.settings.strings = {
+       prefixAgo: "hace",
+       prefixFromNow: "dentro de",
+       suffixAgo: "",
+       suffixFromNow: "",
+       seconds: "menos de un minuto",
+       minute: "un minuto",
+       minutes: "unos %d minutos",
+       hour: "una hora",
+       hours: "%d horas",
+       day: "un día",
+       days: "%d días",
+       month: "un mes",
+       months: "%d meses",
+       year: "un año",
+       years: "%d años"
+    };
+  } else {
+    $.extend($.timeago.settings.strings, {
+      inPast: 'just now',
+      seconds: 'seconds',
+      minute: "1 min",
+      minutes: "%d mins",
+      hour: "an hour",
+      hours: "%d hours",
+      month: "a month",
+      year: "a year",
+    });
+  }
+  $('time').timeago();
 });
