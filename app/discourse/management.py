201a7c2d64f29bc757f90f8b56eee7103d0de97c
Atul Varma <varmaa@gmail.com>
2016-02-22 07:43:15 -0500

    Slightly less horrible implementation of sync_user maybe

--- app/discourse/management.py
+++ app/discourse/management.py
@@ -1,7 +1,6 @@
 import os
 import requests
-from flask import current_app
-from flask.ext.script.commands import InvalidCommand
+from flask.ext.script.commands import InvalidCommand, Command, Option
 from flask_script import Manager
 
 from ..models import User
@@ -10,27 +9,37 @@ from .config import config
 
 DiscourseCommand = manager = Manager(usage='Manage Discourse integration.')
 
-def uses_custom_request_origin(func):
-    return manager.option(
-        '--origin',
-        dest='origin',
-        help='Origin of NoI server (e.g. http://localhost:8080)'
-    )(func)
+class CustomOriginCommand(Command):
+    def __init__(self):
+        super(CustomOriginCommand, self).__init__(self.run_command)
+        self.option_list.append(Option(
+            '--origin',
+            dest='origin',
+            help='Origin of NoI server (e.g. http://localhost:8080)'
+        ))
 
-def custom_request_origin(kwargs):
-    origin = kwargs.get('origin')
-    if origin is None:
-        if os.environ.get('NOI_DEPLOY') == 'production':
-            origin = 'https://%s' % current_app.config['NOI_DEPLOY']
-            print "Inferring server origin is %s." % origin
-            print "If this is incorrect, please use the --origin option."
-        else:
-            raise InvalidCommand(
-                'Cannot determine origin of server; please provide one '
-                'with the --origin option.'
-            )
+    def get_origin(self, app, origin):
+        if origin is None:
+            if os.environ.get('NOI_DEPLOY') == 'production':
+                origin = 'https://%s' % app.config['NOI_DEPLOY']
+                print "Inferring server origin is %s." % origin
+                print "If this is incorrect, please use the --origin option."
+            else:
+                raise InvalidCommand(
+                    'Cannot determine origin of server; please provide one '
+                    'with the --origin option.'
+                )
+        return origin
 
-    return current_app.test_request_context(base_url=origin)
+    def run_command(self):
+        raise NotImplementedError()
+
+    def __call__(self, app=None, *args, **kwargs):
+        origin = self.get_origin(app, kwargs.get('origin'))
+        if 'origin' in kwargs:
+            del kwargs['origin']
+        with app.test_request_context(base_url=origin):
+            return self.run(*args, **kwargs)
 
 def sync_user_with_discourse(user):
     r = requests.post(
@@ -60,16 +69,16 @@ def test_api():
     get_categories(config.admin_username)
     print "Discourse integration works!"
 
-@uses_custom_request_origin
-@manager.command
-def sync_user(username, **kwargs):
-    '''
-    Sync Discourse SSO information about the given user.
-    '''
+class SyncUserCommand(CustomOriginCommand):
+    def run_command(self, username):
+        '''
+        Sync Discourse SSO information about the given user.
+        '''
 
-    user = User.query_in_deployment().filter(User.username==username)[0]
+        user = User.query_in_deployment().filter(User.username==username)[0]
 
-    with custom_request_origin(kwargs):
         sync_user_with_discourse(user)
 
-    print "User %s (%s) synchronized." % (user.username, user.full_name)
+        print "User %s (%s) synchronized." % (user.username, user.full_name)
+
+manager.add_command('sync_user', SyncUserCommand)
