6951412469c1c2b3d214e1e0f23cc0e14bf119d6
Atul Varma <varmaa@gmail.com>
2016-02-23 16:31:34 -0500

    Add tests for discourse.api.

--- app/tests/test_discourse.py
+++ app/tests/test_discourse.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
+import mock
+import flask_testing
+from flask import Flask
 
-from ..discourse import sso
+from ..discourse import sso, api
 
 class HmacSigningTests(TestCase):
     def test_packing_and_unpacking_work_with_valid_signature(self):
@@ -12,3 +15,48 @@ class HmacSigningTests(TestCase):
         signed = sso.pack_and_sign_payload({'hello': 'there'}, secret='foo')
         with self.assertRaises(sso.InvalidSignatureError):
             payload = sso.unpack_and_verify_payload(signed, secret='bar')
+
+@mock.patch('requests.request')
+class ApiTests(flask_testing.TestCase):
+    def create_app(self):
+        app = Flask('discourse_test')
+        app.config['DISCOURSE'] = dict(
+            api_key='abcd',
+            origin='http://discourse',
+            sso_secret='hi',
+        )
+        return app
+
+    def test_request_passes_api_username(self, request):
+        api.request('get', '/blah', username='gu')
+        request.assert_called_once_with(
+            'get',
+            'http://discourse/blah',
+            params=dict(api_key='abcd', api_username='gu'),
+        )
+
+    def test_request_merges_params(self, request):
+        api.request('get', '/blah', params={'hi': 'there'})
+        request.assert_called_once_with(
+            'get',
+            'http://discourse/blah',
+            params=dict(api_key='abcd', api_username='system', hi='there'),
+        )
+
+    def test_get_passes_extra_kwargs(self, request):
+        api.get('/boop', extra=1)
+        request.assert_called_once_with(
+            'get',
+            'http://discourse/boop',
+            params=dict(api_key='abcd', api_username='system'),
+            extra=1
+        )
+
+    def test_post_passes_extra_kwargs(self, request):
+        api.post('/boop', extra=1)
+        request.assert_called_once_with(
+            'post',
+            'http://discourse/boop',
+            params=dict(api_key='abcd', api_username='system'),
+            extra=1
+        )
