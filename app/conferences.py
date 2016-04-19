d1372c0e2913bf583edc6cebaeca465dc3584fe0
Atul Varma <varmaa@gmail.com>
2016-04-19 08:23:17 -0400

    list conferences in deployments.yml.

--- app/conferences.py
+++ app/conferences.py
@@ -5,6 +5,15 @@ from flask import url_for
 STATIC_LOGO_PATH = 'img/conferences'
 FILESYSTEM_LOGO_PATH = '/noi/app/static/' + STATIC_LOGO_PATH
 
+class Conferences(list):
+    @property
+    def featured(self):
+        return [c for c in self if c.is_featured]
+
+    @classmethod
+    def from_yaml(cls, obj):
+        return cls(map(Conference.from_yaml, obj))
+
 class Conference(object):
     def __init__(self, id, name, url, start_date, is_featured=False,
                  logo_filename=None):
@@ -15,6 +24,12 @@ class Conference(object):
         self.is_featured = is_featured
         self.logo_filename = logo_filename or None
 
+        if not isinstance(start_date, datetime.date):
+            raise ValueError('start_date must be a datetime.date')
+
+        if not isinstance(is_featured, bool):
+            raise ValueError('is_featured must be a boolean')
+
         if logo_filename:
             abspath = os.path.join(FILESYSTEM_LOGO_PATH, logo_filename)
             if not os.path.exists(abspath):
@@ -22,8 +37,11 @@ class Conference(object):
 
     @classmethod
     def from_yaml(cls, obj):
-        obj['start_date'] = datetime.datetime.strptime(
-            obj['start_date'],
-            '%Y-%m-%d'
-        ).date()
+        if isinstance(obj['start_date'], basestring):
+            obj['start_date'] = datetime.datetime.strptime(
+                obj['start_date'],
+                '%Y-%m-%d'
+            ).date()
         return cls(**obj)
+
+from_yaml = Conferences.from_yaml
--- app/tests/test_conferences.py
+++ app/tests/test_conferences.py
@@ -2,7 +2,7 @@ import os
 import datetime
 from unittest import TestCase
 
-from ..conferences import Conference, FILESYSTEM_LOGO_PATH
+from ..conferences import Conferences, Conference, FILESYSTEM_LOGO_PATH
 
 if not os.path.exists(FILESYSTEM_LOGO_PATH):
     os.mkdir(FILESYSTEM_LOGO_PATH)
@@ -16,6 +16,14 @@ def conference_from_yaml(**kwargs):
     }
     c.update(kwargs)
     return Conference.from_yaml(c)
+
+class ConferencesTests(TestCase):
+    def test_featured(self):
+        bar = conference_from_yaml(is_featured=False)
+        foo = conference_from_yaml(is_featured=True)
+
+        c = Conferences([bar, foo])
+        self.assertEqual(c.featured, [foo])
 
 class ConferenceTests(TestCase):
     def test_parses_start_date(self):
