798dc418763d0b09283446e10c8535d3c746b185
mocxd <mark.adkins.hastings@gmail.com>
2016-06-20 22:13:53 -0400

    testing js cache issue

--- app/static/js/pages/scroller.js
+++ app/static/js/pages/scroller.js
@@ -4,9 +4,6 @@ $(function () {
     if ($(this).hasClass('js-left')) { x = -50; }
     else if ($(this).hasClass('js-right')) { x = 50; }
 
-    console.log('potat');
-    console.log('biyat');
-
     $($(this).attr('data-target')).scrollLeft(
       $($(this).attr('data-target')).scrollLeft() + x
     );
