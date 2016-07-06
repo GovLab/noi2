8c3e11b024a5a0b47770f57bf8ad8d18d38f3365
mocxd <mark.adkins.hastings@gmail.com>
2016-07-06 10:04:22 -0400

    tweak amount buttons scroll 2

--- app/static/js/pages/scroller.js
+++ app/static/js/pages/scroller.js
@@ -1,10 +1,10 @@
 $(function () {
   $('.js-scroll-btn').click(function(e) {
-    var scrollPixels = 300;
+    var scrollPixels = 100;
     if ($(this).hasClass('js-left')) { scrollPixels *= -1; }
 
     $($(this).attr('data-target')).animate({
         scrollLeft: $($(this).attr('data-target')).scrollLeft() + scrollPixels
-    }, 100, 'swing' );
+    }, 200, 'swing' );
   });
 });
